785fc28a4cf03f39e392dd3a0292740446421da5
alexandrebeugnies <beugniesalexandre@outlook.com>
2022-07-12 10:19:19 +0200

    '

---
+++ src/citations.ts
@@ -0,0 +1,14 @@
+export interface QuoteInterface{
+    author:string,
+    quote:string
+}
+
+//* * Tableau de citations */
+
+export const quotes:QuoteInterface[] = [
+  {
+    author: 'Amaury',
+    quote: "J'arrive dans deux minutes je te stimule",
+  },
+
+];
