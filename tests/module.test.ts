5486efd7042ac4ac4aeef45f4f66f4cf95b51966
alexandrebeugnies <beugniesalexandre@outlook.com>
2022-07-12 11:33:52 +0200

    '

--- tests/module.test.ts
+++ tests/module.test.ts
@@ -1,3 +1,9 @@
-test('It gets a random quote', () => {
+import { getRandomQuote } from '../src'
+import { quotes } from '../src/citations'
 
+test('It gets a random quote', () => {
+  const randomQuote = getRandomQuote()
+
+  //! On vérifie que l'on récupère bien une citation
+  expect(randomQuote).toContain(quotes)
 })
