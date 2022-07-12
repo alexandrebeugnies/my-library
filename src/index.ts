331d0abc7208358ea3af4f13ec2a356144f38294
alexandrebeugnies <beugniesalexandre@outlook.com>
2022-07-12 10:39:35 +0200

    '

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/prefer-default-export */
+import { quotes } from './citations.ts';
+import { randomElement } from './random-element.ts';
 //* * Récupère une citation aléatoire de TRIPTYK  **/
 
 export function getRandomCitation() {
-
+  return randomElement(quotes);
 }
