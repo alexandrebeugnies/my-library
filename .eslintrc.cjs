a8b7b1a3942949f640416d8542da53d29da387a2
alexandrebeugnies <beugniesalexandre@outlook.com>
2022-07-12 11:13:36 +0200

    '

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,24 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
+    node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['standard'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts']
+      },
+    },
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    '@typescript-eslint',
-  ],
+  plugins: ['@typescript-eslint'],
   rules: {
+    // on désactive la règle
     'import/prefer-default-export': 'off',
   },
-};
+},
