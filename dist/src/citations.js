ad7be2d76efc4e0f5f6397675a4c5722f5f3ff20
alexandrebeugnies <beugniesalexandre@outlook.com>
2022-07-12 16:27:58 +0200

    Project init

---
+++ dist/src/citations.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.quotes = void 0;
+//* * Tableau de citations */
+exports.quotes = [
+    {
+        author: 'Amaury',
+        quote: "J'arrive dans deux minutes je te stimule"
+    },
+    {
+        author: 'Amaury',
+        quote: 'La grosse moulaaaaaaaa'
+    },
+    {
+        author: 'Dramix',
+        quote: 'J\'ai beau être matinal,YAML'
+    }
+];
