6d7657aadbc85940be7c11cb8bf8aa533b464cbf
alexandrebeugnies <beugniesalexandre@outlook.com>
2022-07-12 10:20:39 +0200

    '

--- src/citations.ts
+++ src/citations.ts
@@ -10,5 +10,9 @@ export const quotes:QuoteInterface[] = [
     author: 'Amaury',
     quote: "J'arrive dans deux minutes je te stimule",
   },
+  {
+    author: 'Amaury',
+    quote: 'La grosse moulaaaaaaaa',
+  },
 
 ];
