388fd28780e0c1f599708c20c16a73aaff569525
alexandrebeugnies <beugniesalexandre@outlook.com>
2022-07-12 10:37:51 +0200

    '

--- src/random-element.ts
+++ src/random-element.ts
@@ -1,3 +1,5 @@
+//* * Récupère un élément aléatoire dans le tableau */
+
 export function randomElement(array:unknown[]) {
   const index = Math.floor(Math.random() * array.length);
   return array[index];
